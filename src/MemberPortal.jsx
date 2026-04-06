ed58d38926d53649a20b8d85cc6aab87c9557926
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-06 15:20:40 -0400

    Create MemberPortal.jsx

---
+++ src/MemberPortal.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import MemberPass from "../components/MemberPass";
+import MemberServices from "../components/MemberServices";
+
+export default function MemberPortal() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  return authenticated
+    ? <MemberServices />
+    : <MemberPass onAuthenticated={() => setAuthenticated(true)} />;
+}
