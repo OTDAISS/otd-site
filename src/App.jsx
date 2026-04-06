3d4a237f36ebdc9b2c7cfb96318f2f437683e7e8
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-06 15:13:26 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SystemDock from "./components/SystemDock";
-import Header from "./components/Header";
 import "./styles/global.css";
 
 export default function App() {
   return (
-    <div className="w-full h-screen bg-gradient-to-b from-black via-blue-900 to-black text-white relative overflow-hidden">
-      <Header />
+    <div className="w-full min-h-screen bg-gradient-to-b from-black via-blue-900 to-black text-white relative overflow-x-hidden">
       <SystemDock />
-
-      <div className="pt-20">
+      <div className="pt-24">
         <Outlet />
       </div>
     </div>
